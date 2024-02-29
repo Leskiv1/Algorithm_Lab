fe15b5db02a0fb97331ce68cb0b62be0f5019a5b
Nazar Leskiv <Nazareth56778@gmail.com>
2024-02-29 19:26:21 +0200

    Difficulty reduced from O(n*log(n)) to O(n)

--- src/find_unsorted_subarray.py
+++ src/find_unsorted_subarray.py
@@ -1,52 +1,46 @@
-def split_array(array):
-    mid = len(array) // 2
-    left = array[:mid]
-    right = array[mid:]
-    return left, right
-
-
-def mergesort(array):
-    if len(array) < 2:
-        return array
-    left, right = split_array(array)
-
-    left = mergesort(left)
-    right = mergesort(right)
-
-    return merge(left, right)
-
-
-def merge(left, right):
-    left_pos, right_pos = 0, 0
-    result = []
-
-    while left_pos < len(left) and right_pos < len(right):
-        if left[left_pos] < right[right_pos]:
-            result.append(left[left_pos])
-            left_pos += 1
+def find_unsort_arr(array):
+    left_iter = 0
+    right_iter = len(array) - 1
+    while array[left_iter] <= array[right_iter]:
+        if left_iter == right_iter:
+            return -1, -1
+        if array[left_iter] < array[left_iter + 1]:
+           left_iter += 1
+        elif array[right_iter] >= array[right_iter - 1]:
+            right_iter -= 1
         else:
-            result.append(right[right_pos])
-            right_pos += 1
-    result.extend(left[left_pos:])
-    result.extend(right[right_pos:])
-    return result
-
-
-def find_min_max_elements(array):
-    sort_arr = mergesort(array)
-    min_index = -1
-    max_index = -1
-    for i in range(len(array)):
-        if array[i] != sort_arr[i]:
-            min_index = i
             break
-    for i in range(1, len(array)):
-        if array[-i] != sort_arr[-i]:
-            max_index = len(array) - i
+
+    
+    min_el, max_el = find_min_max_elements_in_unsort_arr(array[left_iter : right_iter])
+    
+    for i in range(left_iter - 1, -1, -1):
+        if array[i] > min_el:
+            left_iter -= 1
+        else:
             break
-    return min_index, max_index
+            
+    for i in range (right_iter + 1, len(array)):
+        if array[i] < max_el:
+            right_iter -= 1
+        else:
+            break
+
+    return left_iter, right_iter
+
+def find_min_max_elements_in_unsort_arr(arr):
+    min_el = arr[0]
+    max_el = arr[0]
+    for i in range(1,len(arr) - 1):
+        if min_el > arr[i]:
+            min_el = arr[i]
+    for i in range(1,len(arr)):
+        if max_el < arr[i]:
+            max_el = arr[i]
+    return min_el, max_el
+
 
 
 arr = [5, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 17]
 
-print(find_min_max_elements(arr))
+print(find_unsort_arr(arr))
--- tests/test_find_unsorted_subarray.py
+++ tests/test_find_unsorted_subarray.py
@@ -1,25 +1,25 @@
 import unittest
-from degree_3 import find_min_max_elements
+from Algorithm_Lab.src.find_usorted_subarray import find_unsort_arr
 
 
 class MyTestCase(unittest.TestCase):
     def test_one_element(self):
         array = [1]
-        min_index, max_index = find_min_max_elements(array)
-        self.assertEqual(min_index, -1)
-        self.assertEqual(max_index, -1)
+        self.assertEqual(find_unsort_arr(array), (-1, -1))
 
     def test_is_sorted(self):
         array = [1, 2, 3, 4, 5, 6]
-        min_index, max_index = find_min_max_elements(array)
-        self.assertEqual(min_index, -1)
-        self.assertEqual(max_index, -1)
+        self.assertEqual(find_unsort_arr(array), (-1, -1))
 
     def test_all_need_sorted(self):
         array = [10, 3, 5, 2, 1]
-        min_index, max_index = find_min_max_elements(array)
-        self.assertEqual(min_index, 0)
-        self.assertEqual(max_index, len(array) - 1)
+        self.assertEqual(find_unsort_arr(array), (0, len(array) - 1))
+        
+    def test_different_array(self):
+        array1 = [1, 2, 3, 88, 66, 44, 66, 99, 55, 101, 102, 103, 104, 106, 106]
+        array2 = [1, 3, 10, 7, 5, 2, 6, 10, 12, 15]
+        self.assertEqual(find_not_sort_range(array1), (3, 8))
+        self.assertEqual(find_not_sort_range(array2), (1, 6))
 
 
 if __name__ == '__main__':
