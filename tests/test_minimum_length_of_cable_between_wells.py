a6387055349f9cee43ab374278cb63e6476d3f14
Nazar Leskiv <Nazareth56778@gmail.com>
2024-05-13 22:50:42 +0300

    refactor folder architecture

--- tests/test_minimum_length_of_cable_between_wells.py
+++ tests/test_minimum_length_of_cable_between_wells.py
@@ -4,18 +4,21 @@ from src.minimum_length_of_cable_between_wells import read
 
 class MyTestCase(unittest.TestCase):
     def test_random_vertex(self):
-        result = read('../resources/communication_wells.csv')
+        result = read('../resources/input_files_for_minimum_length_of_cable_between_wells/communication_wells.csv')
         self.assertEqual(result, 37)
 
-        result1 = read('../resources/another_communication_wells.csv')
+        result1 = read(
+            '../resources/input_files_for_minimum_length_of_cable_between_wells/another_communication_wells.csv')
         self.assertEqual(result1, 19)
 
     def test_graph_with_unconnected_wells(self):
-        result = read('../resources/communication_wells_with_unconnected_wells.csv')
+        result = read(
+            '../resources/input_files_for_minimum_length_of_cable_between_wells/communication_wells_with_unconnected_wells.csv')
         self.assertEqual(result, -1)
 
     def test_empty_graph(self):
-        result = read('../resources/communication_wells_with_no_vertex.csv')
+        result = read(
+            '../resources/input_files_for_minimum_length_of_cable_between_wells/communication_wells_with_no_vertex.csv')
         self.assertEqual(result, -1)
 
 
