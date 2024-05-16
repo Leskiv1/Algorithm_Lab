1328fda448c738b79a5f5400247e9517255dba7c
Nazar Leskiv <Nazareth56778@gmail.com>
2024-05-16 23:03:47 +0300

    Fixed the operation of the algorithm, changed the names of the files

--- src/find_the_max_possible_wire_length.py
+++ src/find_the_max_possible_wire_length.py
@@ -6,16 +6,17 @@ import csv
 def find_length_of_electric_cable(w: float, heights: List[float]) -> float:
     """
     We have to find the maximum possible cable length between electric pillars, for this we use the Pythagorean theorem.
-    One of our katet is always fixed (w), so the hypotenuse (cable length) will be always be max,
-    when the other is max. The unknown katet will always be either the height of the current_pillar - 1,
-    or the abs(subtraction of the heights of the current_pillar and next_pillar).
+    One of our katet is always fixed (w), so the hypotenuse (cable length) will be always be max,when the other is max.
+    We have four variants the unknown katet: max_min_between_two_pills, min_max_between_two_pills,
+    max_max_between_two_pills, min_min_between_two_pills
 
     :param w: The distance between the pillars
     :param heights: The maximum possible height of each pillar
     :return: float: the maximum possible cable length between electric pillars, with two digits after the decimal point
     """
 
-    result = 0
+    max_min = 0
+    min_max = 0
     for i in range(0, len(heights) - 1):
         current_pillar = heights[i]
         next_pillar = heights[i + 1]
@@ -23,25 +24,20 @@ def find_length_of_electric_cable(w: float, heights: List[float]) -> float:
         if current_pillar == 0 or next_pillar == 0:
             return 0
 
-        if current_pillar >= next_pillar:
-            length_of_electric_wire = math.sqrt((current_pillar - 1) ** 2 + w ** 2)
-            heights[i + 1] = 1
+        max_min_between_two_pills = min_max + math.sqrt((current_pillar - 1) ** 2 + w ** 2)
+        min_max_between_two_pills = max_min + math.sqrt((next_pillar - 1) ** 2 + w ** 2)
+        max_max_between_two_pills = min_max + math.sqrt(abs(next_pillar - current_pillar) ** 2 + w ** 2)
+        min_min_between_two_pills = max_min + w
 
-        else:
+        min_max = max(min_max_between_two_pills, max_max_between_two_pills)
+        max_min = max(max_min_between_two_pills, min_min_between_two_pills)
 
-            if next_pillar - current_pillar <= current_pillar - 1 or i == 0:
-                length_of_electric_wire = math.sqrt((next_pillar - 1) ** 2 + w ** 2)
-                heights[i] = 1
-
-            else:
-                length_of_electric_wire = math.sqrt((next_pillar - current_pillar) ** 2 + w ** 2)
-
-        result += length_of_electric_wire
+    result = max(min_max, max_min)
 
     return float(f"{result:.2f}")
 
 
-def read(input_file: str) -> List[List[str]]:
+def read(input_file: str) -> (float, List[float]):
     """
     Reads the input CSV file and performs preprocessing of data, where 'w' is the distance between the pillars
     and 'heights' is the maximum possible height of each pillar
--- tests/test_find_the_max_ possible_required_wire_length.py
+++ tests/test_find_the_max_ possible_required_wire_length.py
@@ -4,20 +4,23 @@ from src.find_the_max_possible_wire_length import read
 
 class MyTestCase(unittest.TestCase):
     def test_random_value(self):
-        result = read('../resources/input_files_for_find_the_max_possible_wire_length/random_value.csv')
-        self.assertEqual(result, 8.94)
+        result = read('../resources/input_files_for_find_the_max_possible_wire_length/height_with_value_3.csv')
+        self.assertEqual(5.66, result)
 
-        result1 = read('../resources/input_files_for_find_the_max_possible_wire_length/another_random_value.csv')
-        self.assertEqual(result1, 396.32)
+        result1 = read(
+            '../resources/input_files_for_find_the_max_possible_wire_length/heights_with_value_100_and_2.csv')
+        self.assertEqual(396.32, result1)
+
+        result2 = read('../resources/input_files_for_find_the_max_possible_wire_length/heights_with_random_value.csv')
+        self.assertEqual(2738.18, result2)
 
     def test_all_pillars_have_height_one(self):
         result = read('../resources/input_files_for_find_the_max_possible_wire_length/all_pillars_have_height_one.csv')
-        self.assertEqual(result, 300)
+        self.assertEqual(300, result)
 
     def test_pill_have_height_zero(self):
         result = read('../resources/input_files_for_find_the_max_possible_wire_length/any_pill_have_height_zero.csv')
-        self.assertEqual(result, 0)
-
+        self.assertEqual(0, result)
 
 
 if __name__ == '__main__':
